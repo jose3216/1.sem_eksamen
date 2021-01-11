2d96d3ebfde0223c47149c63389ed9c192463ad3
Jose Hartkopp <josehartkopp@Joses-MacBook-Pro.local>
2021-01-11 23:32:34 +0100

    tema 2 done

--- animation.js
+++ animation.js
@@ -18,11 +18,3 @@ function toggleMenu() {
 		document.querySelector("#burgermenuknap").textContent = "x";
 	}
 }
-
-function plantOpen() {
-	console.log("plantOpen");
-	document.querySelector("#planter").classList.add("clickthrougth")
-	document.querySelector("#bund").classList.add("bundpostion");
-	document.querySelector("#top").classList.add("toppostion");
-	document.querySelector("#jose").classList.add("hide");
-}
